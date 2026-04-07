747e40065208101d66927f9d7772d417f35fb089
Dinesh K Narayanan <dineshknarayanan.official@gmail.com>
2026-04-07 12:41:00 +0530

    Add variable for storage acc module

---
+++ infra/modules/storage_account/variables.tf
@@ -0,0 +1,27 @@
+variable "name" {
+  type = string
+}
+
+variable "resource_group_name" {
+  type = string
+}
+
+variable "location" {
+  type = string
+}
+
+variable "account_tier" {
+  type = string
+}
+
+variable "account_replication_type" {
+  type = string
+}
+
+variable "containers" {
+  type = list(string)
+}
+
+variable "tags" {
+  type = map(string)
+}
