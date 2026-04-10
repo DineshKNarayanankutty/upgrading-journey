bca802d55eff2c7ba42bb6a71f29c7bea73fa62d
Dinesh K Narayanan <dineshknarayanan.official@gmail.com>
2026-04-10 20:02:25 +0530

    correct the code format

--- infra/modules/adf/main.tf
+++ infra/modules/adf/main.tf
@@ -1,6 +1,6 @@
 resource "azurerm_data_factory" "this" {
-  name = var.name
-  location = var.location
+  name                = var.name
+  location            = var.location
   resource_group_name = var.resource_group_name
 
   identity {
