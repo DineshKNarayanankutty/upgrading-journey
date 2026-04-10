32f9b74fba4e56c2c58f94957497afe76c1c021c
Dinesh K Narayanan <dineshknarayanan.official@gmail.com>
2026-04-10 20:05:25 +0530

    correct the code format

--- infra/modules/unity_catalog/main.tf
+++ infra/modules/unity_catalog/main.tf
@@ -1,25 +1,25 @@
 terraform {
   required_providers {
     databricks = {
-        source = "databricks/databricks"
-        version = "~> 1.4"
+      source  = "databricks/databricks"
+      version = "~> 1.4"
     }
   }
 }
 
 resource "databricks_storage_credential" "this" {
-    name = var.name
-    
-    azure_managed_identity {
-        access_connector_id = var.access_connector_id
-    }
+  name = var.name
 
-    comment = "Managed via Terraform"
+  azure_managed_identity {
+    access_connector_id = var.access_connector_id
+  }
+
+  comment = "Managed via Terraform"
 }
 
 resource "databricks_external_location" "bronze" {
   name = "ext-loc-bronze"
-  url = "abfss://bronze@${var.storage_account_name}.dfs.core.windows.net/"
+  url  = "abfss://bronze@${var.storage_account_name}.dfs.core.windows.net/"
 
   credential_name = databricks_storage_credential.this.name
 
@@ -27,19 +27,19 @@ resource "databricks_external_location" "bronze" {
 }
 
 resource "databricks_external_location" "silver" {
-    name = "ext-loc-silver"
-    url = "abfss://silver@${var.storage_account_name}.dfs.core.windows.net/"
+  name = "ext-loc-silver"
+  url  = "abfss://silver@${var.storage_account_name}.dfs.core.windows.net/"
 
-    credential_name = databricks_storage_credential.this.name
+  credential_name = databricks_storage_credential.this.name
 
-    comment = "Silver Layer"  
+  comment = "Silver Layer"
 }
 
 resource "databricks_external_location" "gold" {
-    name = "ext-loc-gold"
-    url = "abfss://gold@${var.storage_account_name}.dfs.core.windows.net/"
+  name = "ext-loc-gold"
+  url  = "abfss://gold@${var.storage_account_name}.dfs.core.windows.net/"
 
-    credential_name = databricks_storage_credential.this.name
+  credential_name = databricks_storage_credential.this.name
 
-    comment = "Gold Layer"  
+  comment = "Gold Layer"
 }
