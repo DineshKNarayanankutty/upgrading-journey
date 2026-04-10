795eb4f3b80f5d1a26122f8f01b6e1a4d5dcb7cc
Dinesh K Narayanan <dineshknarayanan.official@gmail.com>
2026-04-10 20:04:53 +0530

    Assign key vauld admin

--- infra/modules/rbac/main.tf
+++ infra/modules/rbac/main.tf
@@ -1,3 +1,5 @@
+data "azurerm_client_config" "current" {}
+
 resource "azurerm_role_assignment" "blob_contributor" {
   scope                = var.storage_account_id
   role_definition_name = "Storage Blob Data Contributor"
@@ -22,3 +24,15 @@ resource "azurerm_role_assignment" "queue_contributor" {
   principal_id         = var.principal_id
 
 }
+
+resource "azurerm_role_assignment" "vault_admin" {
+  scope                = var.key_vault_id
+  role_definition_name = "Key Vault Administrator"
+  principal_id         = data.azurerm_client_config.current.object_id
+}
+
+# resource "azurerm_role_assignment" "adf_kv_access" {
+#   scope = module.keyvault.id
+#   role_definition_name = "Key Vault Secrets User"
+#   principal_id = module.adf.principal_id
+# }
