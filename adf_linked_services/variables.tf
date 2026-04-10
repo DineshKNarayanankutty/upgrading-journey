606f5f4851d8149620f55ec645091f8102590992
Dinesh K Narayanan <dineshknarayanan.official@gmail.com>
2026-04-10 20:06:32 +0530

    Add variables for linked services for ADF

---
+++ adf_linked_services/variables.tf
@@ -0,0 +1,19 @@
+variable "data_factory_id" {
+    type = string  
+}
+
+variable "storage_account_name" {
+    type = string  
+}
+
+variable "databricks_workspace_url" {
+    type = string  
+}
+
+variable "databricks_pat" {
+    type = string  
+}
+
+variable "cluster_id" {
+    type = string  
+}
