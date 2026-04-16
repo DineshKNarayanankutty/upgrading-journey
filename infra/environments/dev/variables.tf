2d3face694c1da5506d050c30328bc328faff4b5
Dinesh K Narayanan <dineshknarayanan.official@gmail.com>
2026-04-16 19:42:31 +0530

    Add variables

--- infra/environments/dev/variables.tf
+++ infra/environments/dev/variables.tf
@@ -32,3 +32,23 @@ variable "keyvault_name" {
   description = "Key Vault Name"
   type        = string
 }
+
+variable "single_user_name" {
+  type = string
+}
+
+variable "cluster_name" {
+  type = string
+}
+
+variable "spark_version" {
+  type = string
+}
+
+variable "node_type_id" {
+  type = string
+}
+
+variable "autotermination_minutes" {
+  type = number
+}
