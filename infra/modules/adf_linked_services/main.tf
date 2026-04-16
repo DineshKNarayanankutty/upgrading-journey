db84f03c34340ef281d2083e89258002715b57ee
Dinesh K Narayanan <dineshknarayanan.official@gmail.com>
2026-04-16 16:45:52 +0530

    Add adf linked servoces module

--- infra/modules/adf_linked_services/main.tf
+++ infra/modules/adf_linked_services/main.tf
@@ -1,19 +1,29 @@
-resource "azurerm_data_factory_linked_service_data_lake_storage_gen2" "adls" {
-  name = LS_ADLS_Bronze
+resource "azurerm_data_factory_linked_service_key_vault" "keyvault" {
+  name            = "LS_KeyVault"
   data_factory_id = var.data_factory_id
 
-  url = "https://${var.storage_account_name}.dfs.core.windows.net/"
+  key_vault_id = var.key_vault_id
+}
+
+resource "azurerm_data_factory_linked_service_data_lake_storage_gen2" "adls" {
+  name            = "LS_ADLS_Bronze"
+  data_factory_id = var.data_factory_id
+
+  url = "https://${var.storage_account_name}.dfs.core.windows.net"
 
   use_managed_identity = true
 }
 
 resource "azurerm_data_factory_linked_service_azure_databricks" "databricks" {
-  name = "LS_Databricks_Dev"
+  name            = "LS_Databricks_Dev"
   data_factory_id = var.data_factory_id
 
   adb_domain = var.databricks_workspace_url
 
-  access_token = var.databricks_pat
-
   existing_cluster_id = var.cluster_id
+
+  key_vault_password {
+    linked_service_name = azurerm_data_factory_linked_service_key_vault.keyvault.name
+    secret_name         = "databricks-dev-token"
+  }
 }
