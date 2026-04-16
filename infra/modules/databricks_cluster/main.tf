0f1ff0780d42c206016a8f7ab1ae65d5f1d8be89
Dinesh K Narayanan <dineshknarayanan.official@gmail.com>
2026-04-16 19:42:53 +0530

    Remove hardcoded values

--- infra/modules/databricks_cluster/main.tf
+++ infra/modules/databricks_cluster/main.tf
@@ -11,7 +11,8 @@ resource "databricks_cluster" "this" {
   cluster_name = var.cluster_name
   spark_version = var.spark_version
   node_type_id = var.node_type_id
-  autotermination_minutes = 30
+  autotermination_minutes = var.autotermination_minutes
+  single_user_name = var.single_user_name
 
   num_workers = 0
 
