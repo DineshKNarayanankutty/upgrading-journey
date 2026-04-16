d2bf5530aab3637e78c418d3a505ab75bdd5425b
Dinesh K Narayanan <dineshknarayanan.official@gmail.com>
2026-04-16 19:06:58 +0530

    Add ADB cluster creation module

---
+++ infra/modules/databricks_cluster/main.tf
@@ -0,0 +1,28 @@
+terraform {
+  required_providers {
+    databricks = {
+      source  = "databricks/databricks"
+      version = "~> 1.40"
+    }
+  }
+}
+
+resource "databricks_cluster" "this" {
+  cluster_name = var.cluster_name
+  spark_version = var.spark_version
+  node_type_id = var.node_type_id
+  autotermination_minutes = 30
+
+  num_workers = 0
+
+  data_security_mode = "SINGLE_USER"
+
+  spark_conf = {
+    "spark.master" = "local[*, 4]"
+    "spark.databricks.cluster.profile" = "singleNode"
+  }
+
+  custom_tags = {
+    ResourceClass = "SingleNode"
+  }
+}
